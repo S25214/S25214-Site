c101f46b6e7b43a2a5a4b2f3b1283d03ec9f111d
S25214 <vvetpu55y@gmail.com>
2025-02-19 19:48:26 +0700

    fix: update homepage in package.json and set base path in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/S25214-Site/",
+  base: "/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
